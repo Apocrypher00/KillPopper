5605a20029e6f5d10182a1160f2e66b8c757795b
Apocrypher00 <coltonkenney@hotmail.com>
2025-06-16 21:16:11 -0700

    Only watch the temp folder

--- KillPopper.ps1
+++ KillPopper.ps1
@@ -9,7 +9,7 @@
         Therefore, we can leverage clip creation to confirm a player's kills.
 
     .VERSION
-        1.2.2
+        1.2.3
 
     .AUTHOR
         Apocrypher00
@@ -18,14 +18,20 @@
 #### --- Imports --- ###################################################################################################
 
 using namespace System.Windows.Forms
+
+[CmdletBinding()] param ()
+
 Add-Type -AssemblyName System.Windows.Forms
 
 #### --- End Imports --- ###############################################################################################
 
 #### --- Globals --- ###################################################################################################
 
+# Ensure that Information messages are displayed
+$InformationPreference = 'Continue'
+
 # Current version of the script
-$CurrentVersion = "1.2.2"
+$CurrentVersion = "1.2.3"
 
 # Username for current user
 $UserName = $Env:UserName
@@ -63,11 +69,11 @@ function Show-MessageBox {
         [Parameter(Mandatory = $false, Position = 1)]
         [string]
         $Title = "",
-        
+
         [Parameter(Mandatory = $false, Position = 2)]
         [MessageBoxButtons]
         $Button = [MessageBoxButtons]::OK,
-        
+
         [Parameter(Mandatory = $false, Position = 3)]
         [MessageBoxIcon]
         $Icon = [MessageBoxIcon]::Information
@@ -111,7 +117,7 @@ try {
     $host.UI.RawUI.WindowTitle = "KillPopper"
 
     # Start the transcipt
-    Write-Host "Starting transcript..."
+    Write-Information "Starting transcript..."
     try {
         if (-not (Test-Path -Path ".\logs")) {
             Write-Warning "Logs folder missing. Creating now..."
@@ -124,13 +130,13 @@ try {
     }
 
     # Display the script information
-    Write-Host "Script version is:         '$CurrentVersion'"
-    Write-Host "User is:                   '$Username'"
-    Write-Host "User folder is:            '$UserPath'"
-    Write-Host "Highlights temp folder is: '$ClipPath'"
-    Write-Host "Gallery folder is:         '$GalleryPath'"
-    Write-Host "Nvidia app is at:          '$AppPath'"
-    Write-Host "Splash screen is at:       '$SplashPath'"
+    Write-Information "Script version is:         '$CurrentVersion'"
+    Write-Information "User is:                   '$Username'"
+    Write-Information "User folder is:            '$UserPath'"
+    Write-Information "Highlights temp folder is: '$ClipPath'"
+    Write-Information "Gallery folder is:         '$GalleryPath'"
+    Write-Information "Nvidia app is at:          '$AppPath'"
+    Write-Information "Splash screen is at:       '$SplashPath'"
 
     # Check for updates
     try {
@@ -138,14 +144,14 @@ try {
         $LatestVersion = $LatestVersion.Trim()
 
         if ($LatestVersion -gt $CurrentVersion) {
-            Write-Host "New version available: $LatestVersion!" -ForegroundColor Green
+            Write-Warning "New version available: $LatestVersion!"
             Show-MessageBox `
                 -Message "New version available: $LatestVersion!" `
                 -Title "KillPopper" `
                 -Button OK `
                 -Icon Information | Out-Null
         } else {
-            Write-Host "Script is up to date!" -ForegroundColor Green
+            Write-Information "Script is up to date!"
         }
     } catch {
         Write-Warning "Failed to check for updates!"
@@ -160,7 +166,7 @@ try {
             Wait-Exit "Failed to install NuGet!" $_
         }
     } else {
-        Write-Host "NuGet is already installed."
+        Write-Information "NuGet is already installed."
     }
 
     # Check if the BurntToast module is installed
@@ -172,11 +178,11 @@ try {
             Wait-Exit "Failed to install BurntToast!" $_
         }
     } else {
-        Write-Host "BurntToast is already installed."
+        Write-Information "BurntToast is already installed."
     }
 
     # Import the BurntToast module after confirming installation
-    Write-Host "Importing BurntToast..."
+    Write-Information "Importing BurntToast..."
     try {
         Import-Module "BurntToast"
     } catch {
@@ -190,16 +196,18 @@ try {
     }
 
     # Check if the NVIDIA App is installed
+    # TODO: This was necessary when the app was released, but now it seems to use the temp folder
+    # TODO: Maybe we should check both
     # If so, we need to monitor the gallery instead of the temp folder
-    if (Test-Path -Path $AppPath) {
-        Write-Host "NVIDIA App found. Monitoring Gallery instead of Temp folder."
-        $ClipPath = $GalleryPath 
-    }
+    # if (Test-Path -Path $AppPath) {
+    #     Write-Information "NVIDIA App found. Monitoring Gallery instead of Temp folder."
+    #     $ClipPath = $GalleryPath
+    # }
 
     # Check for Clip Path
     if (-not (Test-Path -Path $ClipPath)) {
         try {
-            Write-Host "Temp folder missing. Creating now..."
+            Write-Information "Temp folder missing. Creating now..."
             New-Item -Path $ClipPath -ItemType Directory -Force
         } catch {
             Wait-Exit "Failed to create temp folder!" $_
@@ -213,13 +221,13 @@ try {
 
         # Ignore "Player downed" clips
         if ($FileName -like "*Player downed*") { return }
-        
-        Write-Host "New clip created: $FileName"
+
+        Write-Information "New clip created: $FileName"
         Show-KillNotification
     }
 
     # Create filesystem watcher for MP4 files
-    Write-Host "Creating Watcher..."
+    Write-Information "Creating Watcher..."
     try {
         $Watcher = [System.IO.FileSystemWatcher]::new($ClipPath, "*.mp4")
         $Watcher.EnableRaisingEvents = $true
@@ -228,7 +236,7 @@ try {
     }
 
     # Monitor the folder for new video files
-    Write-Host "Starting Watcher..."
+    Write-Information "Starting Watcher..."
     try {
         Register-ObjectEvent -InputObject $Watcher -EventName "Created" -Action $ClipAction | Out-Null
     } catch {
@@ -236,7 +244,7 @@ try {
     }
 
     # Keep the script running to continuously monitor
-    Write-Host "The Watcher is watching..."
+    Write-Information "The Watcher is watching..."
     while ($true) { Start-Sleep 1 }
 
 } catch {
